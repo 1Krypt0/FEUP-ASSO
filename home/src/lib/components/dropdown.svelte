ae8eb9b736701c24570b00a83158e1af4510a69c
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-03-08 22:59:49 +0000

    Increase dropdown margin to fit better with the page

--- home/src/lib/components/dropdown.svelte
+++ home/src/lib/components/dropdown.svelte
@@ -14,7 +14,7 @@
 
 	{#if visible}
 		<div
-			class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+			class="absolute right-0 z-10 mt-5 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 			role="menu"
 			aria-orientation="vertical"
 			aria-labelledby="menu-button"
