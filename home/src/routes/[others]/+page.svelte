8c4ef603c8ca01751ce56ed4ec6a71c449ce1b6b
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-03-09 17:41:53 +0000

    Create page skeletons for remainder of UI

---
+++ home/src/routes/[others]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home by Iota</title>
+	<meta name="description" content="Home page" />
+</svelte:head>
+
+<section class="bg-black w-full">text</section>
---
+++ home/src/routes/[page=devices]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	let color = 'bg-pink-400';
+</script>
+
+<svelte:head>
+	<title>Home by Iota</title>
+	<meta name="description" content="Home page" />
+</svelte:head>
+
+<section class="bg-pink-500 w-full">text</section>
