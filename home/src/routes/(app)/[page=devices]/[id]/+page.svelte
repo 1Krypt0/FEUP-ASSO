b28b0f4a386ffc6401fe52a091e6f8cd5ef3a3ee
kat-maq <laimirandasantos@gmail.com>
2023-03-13 18:59:19 +0000

    Add title and button to turn off and on light

---
+++ home/src/routes/(app)/[page=devices]/[id]/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+
+    import { page } from '$app/stores';
+
+	export let data;
+
+    const type = $page.params.page;
+    const device = {
+        name: "Bedroom Light",
+        status: "CONNECTED",
+        currentValue: "1"
+    }
+
+    function toggleDevice() {
+		device.currentValue = device.currentValue == "0" ? "1" : "0";
+        console.log("button", device.currentValue);
+        on = !on
+	}
+
+    let on = false;
+
+    $: on_class = !on ? `${data.colors[type][400]} text-white border-white` : `bg-white text-amber-400`;
+
+</script>
+
+<section class="px-5 py-4 md:px-28 md:py-4">
+    <h1 class="text-lights-400 text-4xl md:text-6xl font-bold"> {device.name }</h1>
+    <section class="pt-8">
+
+        <button on:click={toggleDevice} class="{on_class} border-2 font-bold rounded-2xl py-4 px-20">
+            {#if device.currentValue == "0"}
+				Turn On
+            {:else}
+                Turn Off
+            {/if}
+        </button>
+    </section>
+</section>
