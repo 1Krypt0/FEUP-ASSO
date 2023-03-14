b04de22bafffac8a8c7f810ecca329f9fe3774e7
Margarida Vieira <margaridajcv.27@gmail.com>
2023-03-14 13:40:11 +0000

    add error handling when creating device

--- home/src/lib/services/devices.ts
+++ home/src/lib/services/devices.ts
@@ -2,14 +2,24 @@ import { PUBLIC_URL } from '$env/static/public';
 import type { Device } from '$lib/types/device';
 
 export async function createDevice(device: Device) {
-	await fetch(`${PUBLIC_URL}/devices/new`, {
+	const data = await fetch(`${PUBLIC_URL}/devices/new`, {
 		method: 'POST',
 		headers: {
 			Accept: 'application/json',
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify(device)
-	});
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			return data;
+		})
+		.catch((error) => {
+			console.log('Error creating device', error);
+			return { error };
+		});
+
+	return data;
 }
 
 export async function getDevices(type: string): Promise<Device[]> {
@@ -19,7 +29,7 @@ export async function getDevices(type: string): Promise<Device[]> {
 			return data;
 		})
 		.catch((error) => {
-			console.log(error);
+			console.log(`Error fetching devices of type ${type}`, error);
 			return [];
 		});
 
@@ -33,7 +43,7 @@ export async function getDevice(id: string): Promise<Device> {
 			return data;
 		})
 		.catch((error) => {
-			console.log(error);
+			console.log(`Error fetching device with ID ${id}`, error);
 			return {};
 		});
 
