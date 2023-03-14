3410f06c74dc75ada41f56f63eb6f79e418dc516
kat-maq <laimirandasantos@gmail.com>
2023-03-14 15:41:59 +0000

    Fix colour in media and climate pages

--- home/src/routes/(app)/[page=devices]/[id]/+page.svelte
+++ home/src/routes/(app)/[page=devices]/[id]/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 
     import { page } from '$app/stores';
+	import type { PageData } from './$types';
 
-	export let data;
+	export let data: PageData;
 
     const type = $page.params.page;
     const device = {
         name: "Bedroom Light",
         status: "CONNECTED",
-        currentValue: "1"
+        currentValue: "0"
     }
 
     function toggleDevice() {
@@ -19,20 +20,45 @@
 
     let on = false;
 
-    $: on_class = !on ? `${data.colors[type][400]} text-white border-white` : `bg-white text-amber-400`;
+    $: on_lights_class = on ? `${data.colors[type][400]} text-white border-white` : `bg-white text-lights-400`;
+    $: on_media_class = on ? `${data.colors[type][400]} text-white border-white` : `bg-white text-media-400`;
+    $: on_climate_class = on ? `${data.colors[type][400]} text-white border-white` : `bg-white text-climate-400`;
 
 </script>
 
 <section class="px-5 py-4 md:px-28 md:py-4">
-    <h1 class="text-lights-400 text-4xl md:text-6xl font-bold"> {device.name }</h1>
-    <section class="pt-8">
-
-        <button on:click={toggleDevice} class="{on_class} border-2 font-bold rounded-2xl py-4 px-20">
-            {#if device.currentValue == "0"}
-				Turn On
-            {:else}
-                Turn Off
-            {/if}
-        </button>
-    </section>
+    {#if $page.url.pathname.split('/')[1] === "lights"}
+        <h1 class="text-lights-400 text-4xl md:text-6xl font-bold"> {device.name }</h1>
+        <section class="pt-8">
+            <button on:click={toggleDevice} class="{on_lights_class} border-2 font-bold rounded-2xl py-4 px-20">
+                {#if device.currentValue == "0"}
+                    Turn On
+                {:else}
+                    Turn Off
+                {/if}
+            </button>
+        </section>
+    {:else if $page.url.pathname.split('/')[1] === "media"}
+        <h1 class="text-media-400 text-4xl md:text-6xl font-bold"> {device.name }</h1>
+        <section class="pt-8">
+            <button on:click={toggleDevice} class="{on_media_class} border-2 font-bold rounded-2xl py-4 px-20">
+                {#if device.currentValue == "0"}
+                    Turn On
+                {:else}
+                    Turn Off
+                {/if}
+            </button>
+        </section>
+    {:else}
+        <h1 class="text-climate-400 text-4xl md:text-6xl font-bold"> {device.name }</h1>
+        <section class="pt-8">
+            <button on:click={toggleDevice} class="{on_climate_class} border-2 font-bold rounded-2xl py-4 px-20">
+                {#if device.currentValue == "0"}
+                    Turn On
+                {:else}
+                    Turn Off
+                {/if}
+            </button>
+        </section>
+    {/if}
 </section>
