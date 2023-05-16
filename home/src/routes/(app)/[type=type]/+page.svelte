8713bcb147bb45e0446a05c65bbc2bf814e6331d
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-05-16 14:10:49 +0100

    Simplify default page

--- home/src/routes/(app)/[type=type]/+page.svelte
+++ home/src/routes/(app)/[type=type]/+page.svelte
@@ -1,30 +1,13 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import type { Device } from '$lib/types/device';
-	import { page } from '$app/stores';
 	import { PlusIcon } from 'svelte-feather-icons';
-	import DeviceCard from '$lib/components/device-card.svelte';
-
-	export let data: PageData;
-
-	let devices: Device[] = []; //data.devices;
 </script>
 
 <svelte:head>
 	<title>Home by Iota</title>
 </svelte:head>
 
-<section class="w-full {devices.length !== 0 ? 'p-6' : 'h-full flex justify-center items-center'}">
-	<div class={devices.length !== 0 ? 'mb-4' : ''}>
-		<a href={`${$page.url}/create`}>
-			<PlusIcon />
-		</a>
-	</div>
-	{#if devices.length !== 0}
-		<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-8">
-			{#each devices as device}
-				<DeviceCard {data} {device} />
-			{/each}
-		</div>
-	{/if}
+<section class="w-full h-full flex justify-center items-center">
+	<a href="/room/create">
+		<PlusIcon />
+	</a>
 </section>
