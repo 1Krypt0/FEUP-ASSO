c53b707b87367893700d3c6a5d40a67f7d81bf4a
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-05-16 15:23:36 +0100

    Implement device listing per room and category, with device cards

--- home/src/routes/(app)/[type=type]/[id]/+page.svelte
+++ home/src/routes/(app)/[type=type]/[id]/+page.svelte
@@ -1,74 +1,25 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { updateDeviceValue } from '$lib/services/devices';
-	import type { Device } from '$lib/types/device';
+	import DeviceCard from './device-card.svelte';
 	import type { PageData } from './$types';
+	import { MonitorIcon, SunIcon, WindIcon } from 'svelte-feather-icons';
 
 	export let data: PageData;
-	const type = $page.params.page;
-	const device: Device = data.device;
-
-	function toggleDevice() {
-		device.currentValue = device.currentValue == '0' ? '1' : '0';
-		updateDeviceValue(device.id, device.currentValue);
-	}
-
-	$: on_lights_class =
-		device.currentValue === '1'
-			? `${data.colors[type][400]} text-white border-white`
-			: `bg-white text-lights-400`;
-	$: on_media_class =
-		device.currentValue === '1'
-			? `${data.colors[type][400]} text-white border-white`
-			: `bg-white text-media-400`;
-	$: on_climate_class =
-		device.currentValue === '1'
-			? `${data.colors[type][400]} text-white border-white`
-			: `bg-white text-climate-400`;
 </script>
 
-<section class="px-5 py-4 md:px-28 md:py-4">
-	{#if $page.url.pathname.split('/')[1] === 'lights'}
-		<h1 class="text-lights-400 text-4xl md:text-6xl font-bold">{device.name}</h1>
-		<section class="pt-8">
-			<button
-				on:click={toggleDevice}
-				class="{on_lights_class} border-2 font-bold rounded-2xl py-4 px-20"
-			>
-				{#if device.currentValue == '0'}
-					Turn On
-				{:else}
-					Turn Off
-				{/if}
-			</button>
-		</section>
-	{:else if $page.url.pathname.split('/')[1] === 'media'}
-		<h1 class="text-media-400 text-4xl md:text-6xl font-bold">{device.name}</h1>
-		<section class="pt-8">
-			<button
-				on:click={toggleDevice}
-				class="{on_media_class} border-2 font-bold rounded-2xl py-4 px-20"
-			>
-				{#if device.currentValue == '0'}
-					Turn On
-				{:else}
-					Turn Off
-				{/if}
-			</button>
-		</section>
-	{:else}
-		<h1 class="text-climate-400 text-4xl md:text-6xl font-bold">{device.name}</h1>
-		<section class="pt-8">
-			<button
-				on:click={toggleDevice}
-				class="{on_climate_class} border-2 font-bold rounded-2xl py-4 px-20"
-			>
-				{#if device.currentValue == '0'}
-					Turn On
-				{:else}
-					Turn Off
-				{/if}
-			</button>
-		</section>
-	{/if}
+<section class="px-5 w-full py-4 md:px-28 md:py-4">
+	<h1 class="text-lights-400 text-4xl md:text-6xl font-bold">{data.divisionName}</h1>
+	<section class="grid grid-cols-4 gap-16 py-12">
+		{#each data.divisionDevices as device}
+			{#if device.category === 'lights'}
+				<DeviceCard name={device.name} id={device.id}><SunIcon class="stroke-accent" /></DeviceCard>
+			{:else if device.category === 'media'}
+				<DeviceCard name={device.name} id={device.id}
+					><MonitorIcon class="stroke-accent" /></DeviceCard
+				>
+			{:else if device.category === 'climate'}
+				<DeviceCard name={device.name} id={device.id}><WindIcon class="stroke-accent" /></DeviceCard
+				>
+			{/if}
+		{/each}
+	</section>
 </section>
