8c011ca20f1bfa588ed12209f68000cb4a8bea12
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-05-19 12:06:42 +0100

    Remove roomless devices. Redirect to first room in list, if any are present

--- home/src/routes/(app)/+layout.server.ts
+++ home/src/routes/(app)/+layout.server.ts
@@ -5,10 +5,6 @@ export const load = (async () => {
 	// TODO: Change to data fetching when it is implemented
 	const rooms: { name: string; id: number }[] = [
 		{
-			name: 'Roomless Devices',
-			id: 0
-		},
-		{
 			name: 'Room 1',
 			id: 1
 		},
--- home/src/routes/+page.server.ts
+++ home/src/routes/+page.server.ts
@@ -2,5 +2,26 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (() => {
-	throw redirect(303, 'rooms');
+	// TODO: Fetch the rooms and if none is present redirect to this one. Otherwise redirect to the first one that shows up
+	const rooms: { name: string; id: number }[] = [
+		{
+			name: 'Room 1',
+			id: 1
+		},
+		{
+			name: 'Room 2',
+			id: 2
+		},
+		{
+			name: 'Room 3',
+			id: 3
+		}
+	];
+
+	if (rooms.length === 0) {
+		throw redirect(303, 'rooms/create');
+	} else {
+		const first = rooms.at(0)?.id;
+		throw redirect(303, `rooms/${first}`);
+	}
 }) satisfies PageServerLoad;
