29c9038a2e497717227e01166c9d50a9a8baa536
Margarida Vieira <margaridajcv.27@gmail.com>
2023-05-21 10:36:09 +0100

    fix auth pages design

--- home/src/routes/(auth)/recoverPassword/+page.svelte
+++ home/src/routes/(auth)/recoverPassword/+page.svelte
@@ -3,22 +3,19 @@
 
 	const { form, errors, handleChange, handleSubmit } = createForm({
 		initialValues: {
-			email: '',
-			password: ''
+			email: ''
 		},
 		validate: (values) => {
 			const errors = {
-				email: '',
-				password: ''
+				email: ''
 			};
 
 			if (!values.email) errors.email = 'Email is required';
-			if (!values.password) errors.password = 'Password is required';
 
 			return errors;
 		},
 		onSubmit: (values) => {
-			// TODO: complete login
+			// TODO: complete recover password
 			console.log(values);
 		}
 	});
@@ -27,7 +24,7 @@
 <div class="flex justify-center items-center w-full">
 	<form on:submit|preventDefault={handleSubmit} class="flex flex-col sm:items-end">
 		<h1 class="text-4xl font-bold pb-8">
-			<span>Login to your </span><span class="text-primary">Home</span>
+			<span>Recover your </span><span class="text-primary">Home</span>
 		</h1>
 
 		<formgroup class="py-2 flex flex-col sm:flex-row sm:items-center">
@@ -44,35 +41,9 @@
 			<p class="text-xs text-red-600 font-semibold">{$errors.email}</p>
 		{/if}
 
-		<formgroup class="py-2 flex flex-col sm:flex-row sm:items-center">
-			<label class="sm:mr-4" for="password">Password</label>
-			<input
-				class="focus:outline-none rounded-[20px] shadow-md py-1.5 px-2.5"
-				type="password"
-				id="password"
-				on:change={handleChange}
-				bind:value={$form.password}
-			/>
-		</formgroup>
-		{#if $errors.password}
-			<p class="text-xs text-red-600 font-semibold">{$errors.password}</p>
-		{/if}
-
-		<div class="mt-2">
-			<a href="/recoverPassword" class="underline text-gray-400 hover:text-gray-500"
-				>Forgot your password?</a
-			>
-		</div>
-
 		<div class="mt-12">
-			<span
-				>Do not have an account yet? <a
-					href="/register"
-					class="underline text-gray-400 hover:text-gray-500 mr-4">Register</a
-				></span
-			>
 			<button type="submit" class="bg-primary text-white font-bold rounded-full py-3 px-8"
-				>Login</button
+				>Recover Password</button
 			>
 		</div>
 	</form>
