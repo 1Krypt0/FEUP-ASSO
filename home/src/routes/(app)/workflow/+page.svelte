4bbb6a6b24a6cd6df6547acec415ea4a79531fab
Margarida Vieira <margaridajcv.27@gmail.com>
2023-05-22 23:43:46 +0100

    prepare list workflows and specific workflow pages

---
+++ home/src/routes/(app)/workflow/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import WorkflowCard from './workflow-card.svelte';
+
+	// TODO: fetch workflows
+  let workflows : any[] = []
+</script>
+
+<section class="px-5 w-full py-4 md:px-28 md:py-4">
+	<h1 class="text-lights-400 text-4xl md:text-6xl font-bold">Workflows</h1>
+
+	<section
+		class="flex flex-col items-center sm:grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-16 py-12"
+	>
+		{#each workflows as workflow}
+			<WorkflowCard name={workflow.name} id={workflow.id} />
+		{/each}
+	</section>
+</section>
---
+++ home/src/routes/(app)/workflow/workflow-card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Share2Icon } from 'svelte-feather-icons';
+
+	export let name: string;
+	export let id: number;
+</script>
+
+<section class="flex flex-col drop-shadow-lg bg-white rounded-3xl">
+	<div class="text-xl py-6 px-8 flex gap-6 items-center ">
+		<Share2Icon class="stroke-accent" />
+		<p>{name}</p>
+	</div>
+	<a
+		href="/workflow/{id}"
+		class="self-end px-8 mx-5 text-center text-white font-bold my-6  py-3 rounded-full bg-primary"
+		>See more</a
+	>
+</section>
+
