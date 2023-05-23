e6436b6122ea3a24f748e34f02e411b518e2001c
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-05-23 11:52:53 +0100

    Create seeder for categories

--- core/src/main/kotlin/com/iota/core/startup/DatabaseSeeder.kt
+++ core/src/main/kotlin/com/iota/core/startup/DatabaseSeeder.kt
@@ -2,14 +2,17 @@ package com.iota.core.startup
 
 import com.iota.core.dto.device.RequiredProperties
 import com.iota.core.model.Action
+import com.iota.core.model.Category
 import com.iota.core.repository.ActionRepository
+import com.iota.core.repository.CategoryRepository
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class DatabaseSeeder(
-    val actionRepository: ActionRepository
+    val actionRepository: ActionRepository,
+    val categoryRepository: CategoryRepository,
 ) {
 
     private fun seedActions() {
@@ -18,6 +21,12 @@ class DatabaseSeeder(
         addAction("range", "number", true, mutableListOf("min", "max", "step"))
         addAction("rgb", "number")
         addAction("readonly-number", "number", false)
+    }
+
+    private fun seedCategories() {
+        addCategory("Light", "")
+        addCategory("Media", "")
+        addCategory("Climate", "")
     }
 
     private fun addAction(name: String, type: String, updatable: Boolean = true, required: RequiredProperties? = null) {
@@ -34,8 +43,22 @@ class DatabaseSeeder(
         println("Stored ${action.name}")
     }
 
+    private fun addCategory(name: String, icon: String) {
+        if (categoryRepository.findByName(name).isPresent) {
+            println("Already stored $name")
+            return
+        }
+        val category = Category()
+        category.name = name;
+        // TODO: Change to svg
+        category.icon = "";
+        categoryRepository.save(category)
+        println("Stored ${category.name}")
+    }
+
     @EventListener
     fun seed(event: ContextRefreshedEvent) {
         seedActions()
+        seedCategories()
     }
 }
