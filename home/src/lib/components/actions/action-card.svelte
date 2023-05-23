a6c2a0159abbb79aba9c6b5cfdc39fe859cfb0e1
kat-maq <laimirandasantos@gmail.com>
2023-05-23 09:26:18 +0100

    Add buttons to change between color and intensity

--- home/src/lib/components/actions/action-card.svelte
+++ home/src/lib/components/actions/action-card.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-	import { XIcon } from 'svelte-feather-icons';
+	import { DropletIcon, PercentIcon, PowerIcon, XIcon } from 'svelte-feather-icons';
 	import RangeAction from './range-action.svelte';
 	import ColorPickerAction from './color-picker-action.svelte';
 
 	export let name: string;
 	export let roomID: number;
+    
+    let setIntensity = true;
+    function intensityToBeSet() {
+        setIntensity = true;
+    }
+    function colourToBeSet() {
+        setIntensity = false;
+    }
 </script>
 
 <section class="flex-col flex drop-shadow-lg bg-white text-xl px-6 py-6 h-3/6 w-2/6 rounded-3xl">
@@ -13,12 +21,20 @@
             <slot />
             {name}
         </div>
-        <div class="">
+        <div>
             <a href="/room/{roomID}"><XIcon /></a>
         </div>
     </div>
+    <div>
+        <button><PowerIcon/></button>
+        <button on:click={intensityToBeSet}><PercentIcon/></button>
+        <button on:click={colourToBeSet}><DropletIcon/></button>
+    </div>
     <div class="h-full flex-col flex justify-center">
-        <!-- <RangeAction value={60} min={0} max={100} step={1} device_type="light"></RangeAction> -->
-        <ColorPickerAction hex="#00ff00"></ColorPickerAction>
+        {#if setIntensity}
+            <RangeAction value={60} min={0} max={100} step={1} device_type="light"></RangeAction>
+        {:else}
+            <ColorPickerAction hex="#00ff00"></ColorPickerAction>
+        {/if}
     </div>
 </section>
