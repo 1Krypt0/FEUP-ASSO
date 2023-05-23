7a76fcc48a29713e9472dd27242bba0ab0a28f29
Margarida Vieira <margaridajcv.27@gmail.com>
2023-05-23 12:24:42 +0100

    service to delete and update workflow

--- home/src/lib/services/workflows.ts
+++ home/src/lib/services/workflows.ts
@@ -22,20 +22,6 @@ export async function createWorkflow(workflow: Workflow) {
 	return data;
 }
 
-export async function getWorkflows(): Promise<Workflow[]> {
-	const data = await fetch(`${PUBLIC_URL}/workflows`)
-		.then((response) => response.json())
-		.then((data) => {
-			return data;
-		})
-		.catch((error) => {
-			console.error('Error fetching workflows', error);
-			return [];
-		});
-
-	return data;
-}
-
 export async function getWorkflow(id: string): Promise<Workflow> {
 	const data = await fetch(`${PUBLIC_URL}/workflows/${id}`)
 		.then((response) => response.json())
@@ -45,6 +31,59 @@ export async function getWorkflow(id: string): Promise<Workflow> {
 		.catch((error) => {
 			console.error(`Error fetching workflow with ID ${id}`, error);
 			return {};
+		});
+
+	return data;
+}
+
+export async function updateWorkflow(id: string, active: string) {
+	const data = await fetch(`${PUBLIC_URL}/workflows/${id}`, {
+		method: 'PUT',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			active: active
+		})
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			return data;
+		})
+		.catch((error) => {
+			console.error('Error updating workflow status', error);
+			return { error };
+		});
+
+	return data;
+}
+
+export async function deleteWorkflow(id: string) {
+	const data = await fetch(`${PUBLIC_URL}/workflows/${id}`, {
+		method: 'DELETE'
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			return data;
+		})
+		.catch((error) => {
+			console.error('Error deleting workflow', error);
+			return { error };
+		});
+
+	return data;
+}
+
+export async function getWorkflows(): Promise<Workflow[]> {
+	const data = await fetch(`${PUBLIC_URL}/workflows`)
+		.then((response) => response.json())
+		.then((data) => {
+			return data;
+		})
+		.catch((error) => {
+			console.error('Error fetching workflows', error);
+			return [];
 		});
 
 	return data;
